f6358a1eae2360cf0ef244b927a5d76dbf0611f5
Gerardo Tummolo <gerardo.tummolo@hotmail.com>
2020-05-01 14:42:03 +0200

    cahnged from console to gui

--- main.py
+++ main.py
@@ -1,30 +1,19 @@
-import functionality as func
+import gui
 import datasets as dt
-# import gui
-# import tkinter as tkr
+import tkinter as tkr
 
 #Time to add the GUI -> TkInter
 
 if __name__ == "__main__":
 
-    #multithreding to open a loading screen while generating the datasets dictionary
-    datasetDictionary = dt.DatasetsName()
-
-    # root = tkr.Tk()
-    # application = gui.App(root)
-
-    dataset = func.DatasetSelection(datasetDictionary) #see comment in func
-    print(datasetDictionary[dataset][0])
-    structure = dt.DatasetStructure(datasetDictionary[dataset][1])
-
-    url = f'http://ec.europa.eu/eurostat/wdds/rest/data/v2.1/json/en/{dataset}?precision=1'
-    address = func.GetFilter(url, structure)
-
-
-    xValues = []
-    yValues = []
     
-
-    func.GetValues(address, xValues, yValues)
-
-    func.ChartCreate(address,xValues, yValues)
+    datasetDictionary = dt.DatasetsName()
+    url = 'http://ec.europa.eu/eurostat/wdds/rest/data/v2.1/json/en/'
+    root = tkr.Tk()
+    application = gui.App(root, datasetDictionary, url)
+    
+    root.mainloop()
+    try: 
+        root.destroy()
+    finally:
+        pass
