c995ba2d12c3497d614b706216473d8a28e75aec
Gerardo Tummolo <gerardo.tummolo@hotmail.com>
2020-05-05 13:26:53 +0200

    minor change

--- main.py
+++ main.py
@@ -10,10 +10,13 @@ if __name__ == "__main__":
     datasetDictionary = dt.DatasetsName()
     url = 'http://ec.europa.eu/eurostat/wdds/rest/data/v2.1/json/en/'
     root = tkr.Tk()
+    root.geometry('1000x600')
+    root.configure(bg='#509461')
+    root.title('Eurostat Data')
     application = gui.App(root, datasetDictionary, url)
     
     root.mainloop()
     try: 
         root.destroy()
-    finally:
+    except tkr.TclError:
         pass
