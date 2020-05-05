cb9a380443f5c426636431e0c7f519efee0e3a53
Gerardo Tummolo <gerardo.tummolo@hotmail.com>
2020-05-05 13:25:39 +0200

    added Clear function and some adjustments in the UI

--- gui.py
+++ gui.py
@@ -32,7 +32,7 @@ class App:
             """
             self.datasetSelected = func.datasetSelectionGui(self.resultList.get(tkr.ANCHOR))
             self.dataStructure = dt.DatasetStructure(database[self.datasetSelected][1])
-            filters = tkr.Label(frame, text="FILTERS")
+            filters = tkr.Label(frame, text="FILTERS", bg='#509461')
             filters.grid(row=0, column=0, columnspan=2)
             counter = 1
 
@@ -44,47 +44,64 @@ class App:
             """
                 The function populate the url with the filters and the dataset name, running a request for collecting data and generating the chart
             """
-            filterstring = ''
-            for x in self.filters:
-                filterstring = filterstring + x
-            address = f'{urlstring}{self.datasetSelected}?{filterstring}&precision=1'
-            #debugging
-            print(urlstring)
-            print(address)
-            print(self.filters)
-            print(self.datasetSelected)
-            #
-            func.GetValues(address, self.xValues, self.yValues)
-            func.ChartCreate(address, self.xValues, self.yValues)
-            self.filters.clear()
-            self.xValues.clear()
-            self.yValues.clear()
-            
+            if len(self.filters) != 0:
+                filterstring = ''
+                for x in self.filters:
+                    filterstring = filterstring + x
+                address = f'{urlstring}{self.datasetSelected}?{filterstring}&precision=1'
+                #debugging
+                print(urlstring)
+                print(address)
+                print(self.filters)
+                print(self.datasetSelected)
+                #
+                func.GetValues(address, self.xValues, self.yValues)
+                func.ChartCreate(address, self.xValues, self.yValues)
+                self.filters.clear()
+                self.xValues.clear()
+                self.yValues.clear()
+            else:
+                #Not really. Needs to be fixed
+                top = tkr.Toplevel()
+                top.title("Warning")
+                msg = tkr.Message(top, text='No filter has been applied')
+                msg.pack()
+                button = tkr.Button(top, text="OK", command=top.destroy)
+                button.pack()
+                
+        
+        def clearCommand():
+            self.frame.destroy()
+            self.__init__(master, database, url)
 
 
 
         #Label Search
-        self.searchLabel = tkr.Label(master, text="Search: ")
-        self.searchLabel.grid(row=0, column=0)
+        self.searchLabel = tkr.Label(master, text="Search: ", bg='#509461')
+        self.searchLabel.grid(row=0, column=0, padx=5, sticky=tkr.W)
         #Entry for the search
         self.searchBar = tkr.Entry(master, width=50, relief=tkr.FLAT)
-        self.searchBar.grid(row=0, column=1)
+        self.searchBar.grid(row=0, column=1, sticky=tkr.W)
         self.searchBar.bind('<Return>', lambda e: populateList(self.resultList))
         #searchButton
-        self.searchButton = tkr.Button(master, text='Search', bg="#7DE884", relief=tkr.FLAT, width=20, command = lambda: populateList(self.resultList))
-        self.searchButton.grid(row=0, column=2)
+        self.searchButton = tkr.Button(master, text='Search', bg="#A995E8", relief=tkr.FLAT, width=10, command = lambda: populateList(self.resultList))
+        self.searchButton.grid(row=0, column=2, sticky=tkr.E)
         #ListBox
         self.resultList = tkr.Listbox(master, width=90, relief=tkr.FLAT)
-        self.resultList.grid(row=1, column=0, columnspan =3)
+        self.resultList.grid(row=1, column=0, padx=(5,0), pady=5, columnspan =3)
         #Button that run the query
-        self.selectButton = tkr.Button(master, text='Select Dataset', bg="#7DE884", relief=tkr.FLAT, command=lambda: populateFilters(self.frame))
-        self.selectButton.grid(row=2, column=0, padx=5, sticky=tkr.W)
-        #filtersframes
-        self.frame = tkr.Frame(master, width=100)
-        self.frame.grid(row=1, column=6, rowspan=6)
+        self.selectButton = tkr.Button(master, text='Select Dataset', bg="#A995E8", relief=tkr.FLAT, command=lambda: populateFilters(self.frame))
+        self.selectButton.grid(row=2, column=0, padx=5, pady=5, sticky=tkr.W)
         #button datacollection and visualization:
-        self.runQueryButton = tkr.Button(master, text='run query', bg="#7DE884", relief=tkr.FLAT, command = lambda: runQuery(self.url))
-        self.runQueryButton.grid(row=3, column=1)
+        self.runQueryButton = tkr.Button(master, text='run query', bg="#A995E8", relief=tkr.FLAT, command = lambda: runQuery(self.url))
+        self.runQueryButton.grid(row=2, column=1, pady=5)
+        #Clear Button
+        self.clearButton = tkr.Button(master, text='Clear', bg='#A995E8', relief=tkr.FLAT, command= clearCommand)
+        self.clearButton.grid(row=2, column=2, pady=5, sticky=tkr.E)
+        #filtersframes
+        self.frame = tkr.Frame(master, width=100, bg="#509461")
+        self.frame.grid(row=1, column=6, rowspan=6, padx=5)
+
 
 
 class FiltersFrame:
@@ -95,17 +112,18 @@ class FiltersFrame:
 
         def selectFilter(x: str, listSelection: tkr.Listbox):
             filter= f'&{x.lower()}={listSelection.get(tkr.ANCHOR)[0]}'
+            listSelection.itemconfig(tkr.ANCHOR, bg="#E8A366")
             print(filter) #for debugging
             listOfFilters.append(filter)
 
-        self.title = tkr.Label(master, text=str(label))
+        self.title = tkr.Label(master, text=str(label), bg='#509461')
         self.title.grid(row=counter, column=0)
-        self.lister = tkr.Listbox(master, width=50, height=5)
+        self.lister = tkr.Listbox(master, width=50, height=4)
         self.lister.grid(row=counter, column=1)
         for y in datastructure[label]:
             self.lister.insert(tkr.END, y)
-        self.button = tkr.Button(master, text='Select', bg="#7DE884", relief=tkr.FLAT, command = lambda: selectFilter(label, self.lister))
-        self.button.grid(row=counter, column=2)
+        self.button = tkr.Button(master, text='Select', bg="#A995E8", relief=tkr.FLAT, command = lambda: selectFilter(label, self.lister))
+        self.button.grid(row=counter, column=2, padx=5)
     
         
 
