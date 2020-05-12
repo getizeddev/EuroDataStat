1a05db00e28def5fcaf55e0b33369389fad48816
Gerardo Tummolo <gerardo.tummolo@hotmail.com>
2020-05-12 13:56:25 +0200

    Added possibility to sonload the data retrieved in a worksheet

--- gui.py
+++ gui.py
@@ -40,6 +40,8 @@ class App:
                 subframe = FiltersFrame(frame, self.dataStructure, x, counter, self.filters)
                 counter +=1
 
+            self.dowloadButton.configure(state=tkr.NORMAL)
+
         def runQuery(urlstring):
             """
                 The function populate the url with the filters and the dataset name, running a request for collecting data and generating the chart
@@ -60,6 +62,31 @@ class App:
                 self.filters.clear()
                 self.xValues.clear()
                 self.yValues.clear()
+                populateFilters(self.frame)
+            else:
+                #Not really. Needs to be fixed
+                top = tkr.Toplevel()
+                top.title("Warning")
+                msg = tkr.Message(top, text='No filter has been applied')
+                msg.pack()
+                button = tkr.Button(top, text="OK", command=top.destroy)
+                button.pack()
+
+        def downloadData(urlstring):
+            """
+                The function populate the url with the filters and the dataset name, running a request for collecting data and run the donwloadData funtionality
+            """
+            if len(self.filters) != 0:
+                filterstring = ''
+                for x in self.filters:
+                    filterstring = filterstring + x
+                address = f'{urlstring}{self.datasetSelected}?{filterstring}&precision=1'
+                func.GetValues(address, self.xValues, self.yValues)
+                func.DownloadData(self.xValues, self.yValues)
+                self.filters.clear()
+                self.xValues.clear()
+                self.yValues.clear()
+                populateFilters(self.frame)
             else:
                 #Not really. Needs to be fixed
                 top = tkr.Toplevel()
@@ -98,6 +125,9 @@ class App:
         #Clear Button
         self.clearButton = tkr.Button(master, text='Clear', bg='#A995E8', relief=tkr.FLAT, command= clearCommand)
         self.clearButton.grid(row=2, column=2, pady=5, sticky=tkr.E)
+        #dowload button
+        self.dowloadButton = tkr.Button(master, text='Dowload data', bg='#A995E8', relief=tkr.FLAT, state=tkr.DISABLED, command= lambda: downloadData(self.url))
+        self.dowloadButton.grid(row=3, column=0, padx=5, pady=5, sticky=tkr.W)
         #filtersframes
         self.frame = tkr.Frame(master, width=100, bg="#509461")
         self.frame.grid(row=1, column=6, rowspan=6, padx=5)
@@ -115,6 +145,7 @@ class FiltersFrame:
             listSelection.itemconfig(tkr.ANCHOR, bg="#E8A366")
             print(filter) #for debugging
             listOfFilters.append(filter)
+            self.button.configure(state=tkr.DISABLED)
 
         self.title = tkr.Label(master, text=str(label), bg='#509461')
         self.title.grid(row=counter, column=0)
